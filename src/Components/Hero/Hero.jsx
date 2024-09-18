bbcf2e7ca793f77efa305652fbaf4f5e63c832cd
caiman48 <caiman48clm@gmail.com>
2024-09-18 10:38:48 -0400

    changed front name

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className='Hero' id="hero">
          <img src={profile_img} alt="" />
-         <h1><span>I am James Ross, </span> Fullstack developer</h1>
+         <h1><span>James Ross, </span> Fullstack developer</h1>
             <p> I am a fullstack developer with 6 months of experience in web development</p>
             <div className="Hero-action">
             <div className="Hero-connect">Connect with me</div>
